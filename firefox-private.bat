6e54db2e2fd8ccf797ebcbef02d38351635cc8d9
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2021-08-10 11:13:25 +0800

    Create firefox-private.bat

---
+++ firefox-private.bat
@@ -0,0 +1,12 @@
+cd "C:\Program Files\Mozilla Firefox"
+::firefox -private-window  
+::timeout 1
+firefox -private-window  https://zh.stripchat.com/Sabrina-AA
+firefox -private-window  https://zh.stripchat.com/swag_ccpp
+firefox -private-window  https://zh.stripchat.com/Luna_liu
+firefox -private-window  https://zh.stripchat.com/SexyDevil_Eva
+firefox -private-window  https://zh.stripchat.com/SchoolGirl_Dora
+firefox -private-window  https://zh.stripchat.com/AmiliYan
+firefox -private-window  https://zh.stripchat.com/Asian2021
+firefox -private-window  https://zh.stripchat.com/Evabigbb
+firefox -private-window  https://zh.stripchat.com/Unababy
